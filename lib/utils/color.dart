8bf9baf1fae58074cb3b67261fd44e7fdba08fc6
Rizky Eka Haryadi <rizky28eka@Rizkys-MacBook-Pro.local>
2024-08-28 00:53:44 +0700

    added color on utils

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  // Neutral Colors
+class NeutralColor {
   static const Color white = Color(0xFFFFFFFF);
   static const Color gray1 = Color(0xFFF3F5F7);
   static const Color gray2 = Color(0xFFE7EBEF);
@@ -28,52 +27,113 @@ class AppColors {
   static const Color gray23 = Color(0xFF101419);
   static const Color gray24 = Color(0xFF080A0C);
   static const Color black = Color(0xFF000000);
+}
 
-  // Gradient Colors (Example gradients, adjust as needed)
+class BasicColor {
+  // Blue Shades
+  static const Color blue1 = Color(0xFFE2F0FF);
+  static const Color blue2 = Color(0xFFC5DFFF);
+  static const Color blue3 = Color(0xFFA9CDFF);
+  static const Color blue4 = Color(0xFF93BDFF);
+  static const Color blue5 = Color(0xFF5F97FF);
+  static const Color blue6 = Color(0xFF517DDB);
+  static const Color blue7 = Color(0xFF385BB7);
+  static const Color blue8 = Color(0xFF233E93);
+
+  // Green Shades
+  static const Color green1 = Color(0xFFEEFDE2);
+  static const Color green2 = Color(0xFFD8FCC5);
+  static const Color green3 = Color(0xFFBCF6A6);
+  static const Color green4 = Color(0xFFA0ED8D);
+  static const Color green5 = Color(0xFF76E268);
+  static const Color green6 = Color(0xFF50C24C);
+  static const Color green7 = Color(0xFF34A239);
+  static const Color green8 = Color(0xFF21832D);
+
+  // Yellow Shades
+  static const Color yellow1 = Color(0xFFFFFACD);
+  static const Color yellow2 = Color(0xFFFFF39B);
+  static const Color yellow3 = Color(0xFFFFEB69);
+  static const Color yellow4 = Color(0xFFFFE243);
+  static const Color yellow5 = Color(0xFFFFD505);
+  static const Color yellow6 = Color(0xFFDBB303);
+  static const Color yellow7 = Color(0xFFB79202);
+  static const Color yellow8 = Color(0xFF937301);
+
+  // Red Shades
+  static const Color red1 = Color(0xFFFDEDED);
+  static const Color red2 = Color(0xFFFBDADC);
+  static const Color red3 = Color(0xFFF5A3A7);
+  static const Color red4 = Color(0xFFEF6B72);
+  static const Color red5 = Color(0xFFEA3943);
+  static const Color red6 = Color(0xFFE8212B);
+  static const Color red7 = Color(0xFFCB151E);
+  static const Color red8 = Color(0xFF941016);
+
+  // Turquoise Shades
+  static const Color turquoise1 = Color(0xFFDAFEE9);
+  static const Color turquoise2 = Color(0xFFB5FDDB);
+  static const Color turquoise3 = Color(0xFF8FFAD2);
+  static const Color turquoise4 = Color(0xFF72F6D1);
+  static const Color turquoise5 = Color(0xFF45F0D1);
+  static const Color turquoise6 = Color(0xFF32CEC0);
+  static const Color turquoise7 = Color(0xFF22ABAC);
+  static const Color turquoise8 = Color(0xFF167F8B);
+}
+
+class TransparentColor {
+  static const Color blue = Color(0x265F97FF); // 15% opacity
+  static const Color green = Color(0x1476E268); // 8% opacity
+  static const Color yellow = Color(0x14FFD505); // 8% opacity
+  static const Color red = Color(0x14EA3943); // 8% opacity
+  static const Color turquoise = Color(0x1445F0D1); // 8% opacity
+}
+
+class GradientColor {
   static const LinearGradient gradient1 = LinearGradient(
-    colors: [Color(0xFFB2FEFA), Color(0xFF0ED2F7)], // Light blue to darker blue
+    colors: [Color(0xFFB2FEFA), Color(0xFF0ED2F7)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient2 = LinearGradient(
-    colors: [Color(0xFFDB36A4), Color(0xFFFF6FD8)], // Pink to magenta
+    colors: [Color(0xFFDB36A4), Color(0xFFFF6FD8)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient3 = LinearGradient(
-    colors: [Color(0xFFFFA69E), Color(0xFFFF3F8E)], // Orange to Red
+    colors: [Color(0xFFFFA69E), Color(0xFFFF3F8E)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient4 = LinearGradient(
-    colors: [Color(0xFFFFD54F), Color(0xFFFF4081)], // Yellow to red
+    colors: [Color(0xFFFFD54F), Color(0xFFFF4081)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient5 = LinearGradient(
-    colors: [Color(0xFF00D2FF), Color(0xFF928DAB)], // Blue to grey
+    colors: [Color(0xFF00D2FF), Color(0xFF928DAB)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient6 = LinearGradient(
-    colors: [Color(0xFFEE9CA7), Color(0xFFFFDDE1)], // Pink to peach
+    colors: [Color(0xFFEE9CA7), Color(0xFFFFDDE1)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient7 = LinearGradient(
-    colors: [Color(0xFF7F7FD5), Color(0xFF86A8E7)], // Purple to light blue
+    colors: [Color(0xFF7F7FD5), Color(0xFF86A8E7)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
   static const LinearGradient gradient8 = LinearGradient(
-    colors: [Color(0xFFFFF200), Color(0xFFFF6FD8)], // Yellow to pink
+    colors: [Color(0xFFFFF200), Color(0xFFFF6FD8)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
